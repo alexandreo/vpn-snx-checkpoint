b6f7ee1435e5d55b2aeea222aae9bf26bd081f3a
Alexandre Oliveira <alexandre.oliveira@INTERAKTIV.local>
2018-06-05 14:03:53 -0300

    fix ubuntu version

--- scripts/snx.sh
+++ scripts/snx.sh
@@ -4,62 +4,16 @@ user=$SNX_USER
 password=$SNX_PASSWORD
 
 /usr/bin/expect <<EOF
-set timeout 10
-log_user 0
-send_user "Disconnecting old sessions...\n"
-spawn snx -d
-expect {
-        timeout { exit 2 }
-        eof {
-               sleep 1
-               send_user "Connecting to VPN...\n"
-               spawn -ignore HUP /bin/sh -c "snx -s $server -p 443 -u $user"
-               expect {
-                       "password:" {
-                                      send "$password\r"
-                                      expect {
-                                              "Do you accept" {
-                                                   send_user "Accepting certificate\n"
-                                                   send "y\r"
-                                                   exp_continue
-                                              }
-                                              "connected." {
-                                                    send_user "Connected!\n"
-                                                    exit 0
-                                              }
-                                              "Connection aborted." {
-                                                    send_user "Connection aborted.\n"
-                                                    exit 0
-                                              }
-                                              "Authentication failed" {
-                                                   send_user "Auth failed.\n"
-                                                   exit 0
-                                              }
-                                              eof {
-                                                   send_user "Error!\n"
-                                                   exit 0
-                                              }
-                                              timeout {
-                                                   send_user "Timeout!\n"
-                                                   exit 0
-                                              }
-                                      }
-                                   }
-                      eof {
-                           send_user "Error!\n"
-                           exit 0
-                      }
-                      timeout {
-                            send_user "Timeout!\n"
-                            exit 0
-                      }
-              }
-       }
-}
-expect eof
-exit 1
-
-}
+spawn snx -s $server -u $user
+expect "*?assword:"
+send "$password\r"
+expect "*Do you accept*"
+send "y\r"
+expect "SNX - connected."
+interact
 EOF
+
+#iptables -t nat -A POSTROUTING -o tunsnx -j MASQUERADE
+#iptables -A FORWARD -i eth0 -j ACCEPT
 
 /bin/bash
