27896629ba0287fd7761b125610d2fd54b7c23b7
Alexandre Oliveira <alexandre.oliveira@INTERAKTIV.local>
2018-06-05 11:58:27 -0300

    first commit

---
+++ scripts/snx-run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# vars
+DOCKER_NAME="snx-dante"
+DOCKER_SOURCE="mnasiadka/snx-dante"
+SNX_USER="EDITME"
+SNX_SERVER="EDITME"
+
+while [[ $# -gt 0 ]]
+do
+key=$1
+
+case $key in
+    -l|--logs)
+    	LOGS="1"
+    	shift # past argument
+    ;;
+    -h|--help)
+	echo "Usage: --logs (shows docker logs after run)"
+	exit 0
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+IFS= read -s  -p "SNX Password:" pwd
+echo -e "\n" 
+
+if [ "$(docker ps -aq -f status=running -f name=$DOCKER_NAME)" ]; then
+	echo -ne "$DOCKER_NAME is running, stopping... "
+	docker stop $DOCKER_NAME
+	echo "done"
+fi
+if [ "$(docker ps -aq -f status=exited -f name=$DOCKER_NAME)" ]; then
+	echo -ne "$DOCKER_NAME is not running, removing stale container... "
+        docker rm $DOCKER_NAME
+	echo "done"
+fi
+echo -ne "Starting $DOCKER_NAME..."
+docker run -p 127.0.0.1:1080:1080 --cap-add=ALL -v /lib/modules:/lib/modules -v /dev:/dev -e SNX_PASSWORD=$pwd -e SNX_USER=$SNX_USER -e SNX_SERVER=$SNX_SERVER --name snx-dante -d -ti $DOCKER_SOURCE
+echo "done"
+
+if [ "$LOGS" ]; then
+	echo "LOGS:"
+	docker logs $DOCKER_NAME
+fi
